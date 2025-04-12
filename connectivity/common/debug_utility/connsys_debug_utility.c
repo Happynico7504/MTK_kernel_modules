62b0c8b3a9b1b3bf9ca71bb0350b73b02ff35059
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-12 13:19:21 +0200

    Update connsys_debug_utility.c

--- connectivity/common/debug_utility/connsys_debug_utility.c
+++ connectivity/common/debug_utility/connsys_debug_utility.c
@@ -1114,13 +1114,7 @@ extern int connsys_log_deinit(int conn_type)
 * RETURNS
 *  unsigned int    Ring buffer unread size
 *****************************************************************************/
-unsigned int connsys_log_get_buf_size(int conn_type)
-{
-	if (conn_type >= CONNLOG_TYPE_END || conn_type < 0)
-		return -1;
-	return RING_SIZE(&connlog_buffer_table[conn_type].ring_cache);
-}
-EXPORT_SYMBOL(connsys_log_get_buf_size);
+extern unsigned int connsys_log_get_buf_size(int conn_type)
 
 /*****************************************************************************
 * FUNCTION
@@ -1133,14 +1127,7 @@ EXPORT_SYMBOL(connsys_log_get_buf_size);
 * RETURNS
 *  int    0=success, others=error
 *****************************************************************************/
-int connsys_log_register_event_cb(int conn_type, CONNLOG_EVENT_CB func)
-{
-	if (conn_type >= CONNLOG_TYPE_END || conn_type < 0)
-		return -1;
-	event_callback_table[conn_type] = func;
-	return 0;
-}
-EXPORT_SYMBOL(connsys_log_register_event_cb);
+extern int connsys_log_register_event_cb(int conn_type, CONNLOG_EVENT_CB func)
 
 /*****************************************************************************
 * FUNCTION
@@ -1154,38 +1141,7 @@ EXPORT_SYMBOL(connsys_log_register_event_cb);
 * RETURNS
 *  ssize_t    read buffer size
 *****************************************************************************/
-ssize_t connsys_log_read(int conn_type, char *buf, size_t count)
-{
-	unsigned int written = 0;
-	unsigned int cache_buf_size;
-	struct ring_segment ring_seg;
-	struct ring *ring;
-	unsigned int size = 0;
-
-	if (conn_type < 0 || conn_type >= CONNLOG_TYPE_END)
-		return 0;
-
-	ring = &connlog_buffer_table[conn_type].ring_cache;
-
-	if (atomic_read(&log_mode) != LOG_TO_FILE)
-		goto done;
-
-	size = count < RING_SIZE(ring) ? count : RING_SIZE(ring);
-	if (RING_EMPTY(ring) || !ring_read_prepare(size, &ring_seg, ring)) {
-		pr_err("type(%d) no data, possibly taken by concurrent reader.\n", conn_type);
-		goto done;
-	}
-	cache_buf_size = ring_seg.remain;
-
-	RING_READ_FOR_EACH(size, ring_seg, ring) {
-		memcpy(buf + written, ring_seg.ring_pt, ring_seg.sz);
-		cache_buf_size -= ring_seg.sz;
-		written += ring_seg.sz;
-	}
-done:
-	return written;
-}
-EXPORT_SYMBOL(connsys_log_read);
+extern ssize_t connsys_log_read(int conn_type, char *buf, size_t count)
 
 /*****************************************************************************
 * FUNCTION
@@ -1199,47 +1155,8 @@ EXPORT_SYMBOL(connsys_log_read);
 * RETURNS
 *  ssize_t    read buffer size
 *****************************************************************************/
-ssize_t connsys_log_read_to_user(int conn_type, char __user *buf, size_t count)
-{
-	int retval;
-	unsigned int written = 0;
-	static DEFINE_RATELIMIT_STATE(_rs, 10 * HZ, 1);
-	unsigned int cache_buf_size;
-	struct ring_segment ring_seg;
-	struct ring *ring;
-	unsigned int size = 0;
+extern ssize_t connsys_log_read_to_user(int conn_type, char __user *buf, size_t count)
 
-	ratelimit_set_flags(&_rs, RATELIMIT_MSG_ON_RELEASE);
-
-	if (conn_type < 0 || conn_type >= CONNLOG_TYPE_END)
-		return 0;
-
-	ring = &connlog_buffer_table[conn_type].ring_cache;
-
-	if (atomic_read(&log_mode) != LOG_TO_FILE)
-		goto done;
-
-	size = count < RING_SIZE(ring) ? count : RING_SIZE(ring);
-	if (RING_EMPTY(ring) || !ring_read_prepare(size, &ring_seg, ring)) {
-		pr_err("type(%d) no data, possibly taken by concurrent reader.\n", conn_type);
-		goto done;
-	}
-	cache_buf_size = ring_seg.remain;
-
-	RING_READ_FOR_EACH(size, ring_seg, ring) {
-		retval = copy_to_user(buf + written, ring_seg.ring_pt, ring_seg.sz);
-		if (retval) {
-			if (__ratelimit(&_rs))
-				pr_err("copy to user buffer failed, ret:%d\n", retval);
-			goto done;
-		}
-		cache_buf_size -= ring_seg.sz;
-		written += ring_seg.sz;
-	}
-done:
-	return written;
-}
-EXPORT_SYMBOL(connsys_log_read_to_user);
 
 /*****************************************************************************
 * FUNCTION
