d7dd65a03189cd8a0db71543f72247b89aae988d
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-13 02:23:14 +0200

    Update consys_hw.c

--- connectivity/conninfra/platform/consys_hw.c
+++ connectivity/conninfra/platform/consys_hw.c
@@ -846,7 +846,7 @@ int consys_hw_init(struct conninfra_dev_cb *dev_cb)
 		pr_err("Conninfra platform driver registered failed(%d)\n", iRet);
 	else {
 		if (atomic_read(&g_hw_init_done) == 0) {
-			printk(int retry);
+			printk(KERN_INFO "Retry count: %d\n", retry);
                         pr_warn("conninfra: Skipping wait for g_hw_init_done (no DT?)");
                         atomic_set(&g_hw_init_done, 1);  // fake init done
                 }
