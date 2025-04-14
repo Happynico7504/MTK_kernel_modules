571a02c259de18eee363be12de4300cf3adbb268
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-14 21:06:56 +0200

    Update wmt_cdev_wifi.c

--- connectivity/wlan/adaptor/wmt_cdev_wifi.c
+++ connectivity/wlan/adaptor/wmt_cdev_wifi.c
@@ -885,8 +885,3 @@ module_init(WIFI_init);
 module_exit(WIFI_exit);
 
 #endif
-
-EXPORT_SYMBOL(wifi_pwr_on_init);
-EXPORT_SYMBOL(wifi_pwr_on_deinit);
-EXPORT_SYMBOL(mtk_wcn_wlan_func_ctrl);
-EXPORT_SYMBOL(g_fgIsWiFiOn);
